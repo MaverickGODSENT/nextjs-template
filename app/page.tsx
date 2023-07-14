733f225577d43067c2d3af1919d9412f01b5d705
MaverickGODSENT <galinivaov88@gmail.com>
2023-07-14 17:12:32 +0300

    update
    asd

--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,11 @@ export default function Home() {
         <div className="column-half">
           <h2 className="section-title">Some more info</h2>
           <p>
-          Razer is not only known for its gaming hardware but also for its commitment to esports and gaming communities. The company actively sponsors and collaborates with professional esports teams, players, and events, supporting the growth and development of competitive gaming. Razer's dedication to fostering a sense of community extends to initiatives such as Razer Insider, a platform that allows fans and gamers to connect, share insights, and participate in discussions.
+          Razer is not only known for its gaming hardware but also for its commitment to esports and gaming communities. The company actively sponsors and collaborates with professional esports teams, players, and events, supporting the growth and development of competitive gaming. Razer&apos;s dedication to fostering a sense of community extends to initiatives such as Razer Insider, a platform that allows fans and gamers to connect, share insights, and participate in discussions.
 
-In recent years, Razer has expanded its product lineup to include lifestyle and productivity offerings. This includes Razer Blade laptops, which combine powerful performance with sleek design for professionals and content creators, as well as Razer Phone, a mobile device designed specifically for gaming enthusiasts. Razer's foray into lifestyle products showcases the brand's versatility and commitment to delivering innovative solutions beyond gaming.
+In recent years, Razer has expanded its product lineup to include lifestyle and productivity offerings. This includes Razer Blade laptops, which combine powerful performance with sleek design for professionals and content creators, as well as Razer Phone, a mobile device designed specifically for gaming enthusiasts. Razer&apos;s foray into lifestyle products showcases the brand&apos;s versatility and commitment to delivering innovative solutions beyond gaming.
 
-Razer also prioritizes sustainability and environmental responsibility. The company has implemented initiatives to reduce its environmental footprint, including packaging made from recycled materials and ongoing efforts to minimize waste. Razer's commitment to sustainability aligns with its broader mission to create a positive impact in the gaming industry and beyond.
+Razer also prioritizes sustainability and environmental responsibility. The company has implemented initiatives to reduce its environmental footprint, including packaging made from recycled materials and ongoing efforts to minimize waste. Razer&apos;s commitment to sustainability aligns with its broader mission to create a positive impact in the gaming industry and beyond.
           </p>
         </div>
         <div className="column-half">
@@ -38,7 +38,7 @@ Razer also prioritizes sustainability and environmental responsibility. The comp
       <div className="container">
         <h2 className="section-title">Conclusion</h2>
         <p>
-        Overall, Razer's relentless pursuit of technological advancements, focus on community engagement, and commitment to sustainability have solidified its position as a prominent and influential brand in the gaming world. Continually pushing boundaries and embracing new opportunities, Razer remains dedicated to shaping the future of gaming and enhancing the lives of gamers around the globe.
+        Overall, Razer&apos;s relentless pursuit of technological advancements, focus on community engagement, and commitment to sustainability have solidified its position as a prominent and influential brand in the gaming world. Continually pushing boundaries and embracing new opportunities, Razer remains dedicated to shaping the future of gaming and enhancing the lives of gamers around the globe.
         </p>
       </div>
 
